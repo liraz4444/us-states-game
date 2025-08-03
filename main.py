528181f3feb3f6069e3efc4f0a88a6375feaeb6c
liraz4444 <lirazakiva4444@gmail.com>
2025-08-03 18:14:00 +0300

    First commit - U.S. States Game

---
+++ main.py
@@ -0,0 +1,25 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+guessed_states = []
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)} / 50 States Correct", prompt="What is another state's name?").title()
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
+
+turtle.mainloop()
