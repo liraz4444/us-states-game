69416aba23b1c9c0aeed1b966bc3273d2b060fa7
liraz4444 <lirazakiva4444@gmail.com>
2025-08-05 13:59:09 +0300

    update 5.8

--- main.py
+++ main.py
@@ -13,7 +13,15 @@ all_states = data.state.to_list()
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)} / 50 States Correct", prompt="What is another state's name?").title()
 
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
     if answer_state in all_states:
+
         guessed_states.append(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
